a07776ab7535eb5b893985ae6ae12468b47de354
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2019-10-21 07:57:26 -0400

    added new web api project which uses an alternative way of versioning.

---
+++ WebApiVersioningTwo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+
+namespace WebApiVersioningTwo.Controllers
+{
+    [RoutePrefix("api")]
+    public class HomeController : ApiController
+    {
+        // GET api/<controller>
+        [Route("v1/home")]
+        public IHttpActionResult Get()
+        {
+            return Ok(new {Version = "Version 1" });
+        }
+    }
+
+    [RoutePrefix("api")]
+    public class HomeV2Controller : ApiController
+    {
+        // GET api/<controller>
+        [Route("v2/home")]
+        public IHttpActionResult Get(string name)
+        {
+            return Ok(new { Version = "Version 2", Name = name });
+        }
+    }
+}
