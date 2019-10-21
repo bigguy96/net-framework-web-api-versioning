3aa7a9d50b5a8b0c2a6066b3bb1d1eef4afcb2e2
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2019-10-21 08:13:37 -0400

    Added new controller to test different version

---
+++ WebApiVersioning/Controllers/HelloController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using Microsoft.Web.Http;
+
+namespace WebApiVersioning.Controllers
+{
+    [ApiVersion("1.0")]
+    [RoutePrefix("api")]
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        [Route("v{version:apiVersion}/hello")]
+        public IHttpActionResult Index(string name)
+        {
+            return Ok(new { Message = $"Hello {name} Version 1" });
+        }
+    }
+
+    [ApiVersion("2.0")]
+    [ApiVersion("3.0")]
+    [RoutePrefix("api")]
+    public class HelloV2Controller : ApiController
+    {
+        [HttpGet]
+        [Route("v{version:apiVersion}/hello")]
+        public IHttpActionResult IndexV2(string name)
+        {
+            return Ok( new { Message = $"Hello {name} Version 2"});
+        }
+
+        [HttpGet, MapToApiVersion("3.0")]
+        [Route("v{version:apiVersion}/hello")]
+        public IHttpActionResult IndexV3(string name)
+        {
+            return Ok(new { Message = $"Hello {name} Version 3" });
+        }
+    }
+}
