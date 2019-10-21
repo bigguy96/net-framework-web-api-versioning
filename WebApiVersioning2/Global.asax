2b689cde46e32d63f6520dae3f8c16ab87c2b66c
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2019-10-21 07:36:10 -0400

    Removed unused folders and added new web api application.

---
+++ WebApiVersioning2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="WebApiVersioning2.WebApiApplication" Language="VB" %>
---
+++ WebApiVersioning2/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("WebApiVersioning2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Transport Canada / Transports Canada")>
+<Assembly: AssemblyProduct("WebApiVersioning2")>
+<Assembly: AssemblyCopyright("Copyright © Transport Canada / Transports Canada 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("577ac479-b987-4532-bcc0-79727f96176c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
